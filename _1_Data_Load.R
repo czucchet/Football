b2889fae660cb8c468ab7414f571bae20502386d
Your Name <czucchet02@hotmail.com>
2018-12-31 12:46:47 +0000

    _1_Data_Load_1.0.1

--- _1_Data_Load.R
+++ _1_Data_Load.R
@@ -22,9 +22,12 @@ get_PL_fixture = function(start, end, league = "ALL",...){
              FT_Home = str_sub(Score,1,1),FT_Away = str_sub(Score,3,3),
              HT_Home = str_sub(Score,6,6),HT_Away = str_sub(Score,8,8),
              Day_of_Game = na.locf(Day_of_Game),Has_Finished = na.locf(Has_Finished),Round_No = na.locf(Round_No),
-             Season_ID = paste0(years_in_scope$start_year[i],"-",years_in_scope$end_year[i]),
-             Teams_ID = paste0(str_replace(Home_Team, " ","-"),Dash,str_replace(Away_Team, " ","-"),"/"),
-             web_ID = paste0("https://www.worldfootball.net/report/premier-league-",Season_ID,"-",Teams_ID)
+             Season_ID = paste0(years_in_scope$start_year[i],"/",years_in_scope$end_year[i]),
+             Teams_ID = str_replace_all(paste0(str_replace_all(Home_Team, " ","-"),Dash,str_replace_all(Away_Team, " ","-"),"/"), "&-",""),
+             web_ID = paste0("https://www.worldfootball.net/report/premier-league-",Season_ID,"-",Teams_ID),
+             FT_Home = as.numeric(FT_Home),FT_Away = as.numeric(FT_Away),
+             Winner = ifelse(FT_Home > FT_Away, "Home",ifelse(FT_Home < FT_Away, "Away", "Draw")),
+             Diff = FT_Home - FT_Away
               ) %>% filter(Dash == "-") %>% select(-Round,-Teams_ID)
     con = dbConnect(SQLite(), "Football_Records.sqlite")
     db_check = try(dbGetQuery(con, "SELECT * FROM Fixture_Detail"),silent = TRUE)
@@ -46,12 +49,15 @@ get_PL_fixture = function(start, end, league = "ALL",...){
 get_PL_games = function(year){
   library(rvest);library(tidyverse);library(DBI);library(RSQLite);library(purrr);library(zoo);library(stringr)
   con = dbConnect(SQLite(), "Football_Records.sqlite")
-  year = paste("SELECT * FROM Fixture_Detail WHERE Season_ID = ",year)
+  year_query = "SELECT * FROM Fixture_Detail WHERE Season_ID = '2017-2018'"
+  test =  dbGetQuery(con, year_query)
   all_fixtures =  dbGetQuery(con, "SELECT * FROM Fixture_Detail")
+  all_fixtures =  dbGetQuery(con, year_query)  
   for(i in 1:length(all_fixtures$web_ID)){
   game = read_html(all_fixtures$web_ID[i]) %>% html_table(fill = TRUE)
   select_game = sapply(game, function(x) nrow(x));find_game = which(select_game > 12 & select_game < 22)
   game_temp = game[find_game]
+  if(length(game_temp) != 0) {
   names(game_temp[[1]]) = c("Number","Player","Subbed_Time")
   game_temp[[1]] = game_temp[[1]] %>% 
     mutate(Is_Starting = 1,Team = rep(all_fixtures[i, "Home_Team"], nrow(game_temp[[1]])),Is_Home = 1,
@@ -67,7 +73,7 @@ get_PL_games = function(year){
       )
     game_temp[[2]][which(str_sub(game_temp[[2]]$Subbed_Time,1,3) == "Sub"):nrow(game_temp[[2]]),"Is_Starting"] <- 0
     game = bind_rows(game_temp)
-    game = game[game$Was_Subbed != "Substitutes",] 
+    game = game[game$Subbed_Time != "Substitutes",] 
     game$Player = str_replace_all(game$Player, "[\r\n\t]" , "")
     db_check = try(dbGetQuery(con, "SELECT * FROM Player_Game_Detail"),silent = TRUE)
     if(class(db_check) == "try-error"){
@@ -81,28 +87,22 @@ get_PL_games = function(year){
       all_records_temp =  dbGetQuery(con, "SELECT * FROM Player_Game_Detail")
       all_records = unique(all_records_temp)
       dbWriteTable(con, "Player_Game_Detail", all_records,overwrite = T)
+          }
         }
       }
-  }
+    }
 
-    
-    
-    for(j in 1:length(game_temp)){
-    game_temp_df = data.frame(game_temp[j])
-    names(game_temp_df) = c("Number","Player","Was_Subbed")
-    game_temp[j] = game_temp_df
-    game_temp[j] = game_temp[j] %>% 
-      mutate(Is_Starting = 1,
-             all_fixtures[i,Home_Team]
-      )   
-  }
+all_records_temp =  dbGetQuery(con, "SELECT * FROM Player_Game_Detail")
+test =  dbGetQuery(con, "SELECT * FROM Fixture_Detail")
+dim(all_records_temp)
 
-  
-  
-  
-  names(fixture_temp) = c("Day_of_Game","Time_of_Game", "Home_Team","Dash","Away_Team","Score","Has_Finished","Round");fixture_temp[1, "Round"] <- "1. Round"
-  
-    
-} 
-  
+length(unique(all_records_temp$GameID))
+
+test[10:20,]
+dim(test)
+
+
+dbListTables(con)
+dbRemoveTable(con,"Fixture_Detail")
+dbRemoveTable(con,"Player_Game_Detail")
 
